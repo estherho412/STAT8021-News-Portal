4f86159221d0a7a2201134d21c1645bd8493c835
Thomas-CTH <thomaschow1999@gmail.com>
2024-04-27 12:52:54 +0800

    llama2

---
+++ llama2/llama2_finetuning.py
@@ -0,0 +1,152 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, HfArgumentParser, TrainingArguments, pipeline,logging
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+
+model_name = "NousResearch/llama-2-7b-chat-hf" # use this if you have access to the official LLaMA 2 model "meta-llama/Llama-2-7b-chat-hf", though keep in mind you'll need to pass a Hugging Face key argument
+dataset_name = "/content/train.jsonl"
+new_model = "llama-2-7b-custom"
+lora_r = 64
+lora_alpha = 16
+lora_dropout = 0.1
+use_4bit = True
+bnb_4bit_compute_dtype = "float16"
+bnb_4bit_quant_type = "nf4"
+use_nested_quant = False
+output_dir = "./results"
+num_train_epochs = 1
+fp16 = False
+bf16 = False
+per_device_train_batch_size = 4
+per_device_eval_batch_size = 4
+gradient_accumulation_steps = 1
+gradient_checkpointing = True
+max_grad_norm = 0.3
+learning_rate = 2e-4
+weight_decay = 0.001
+optim = "paged_adamw_32bit"
+lr_scheduler_type = "constant"
+max_steps = -1
+warmup_ratio = 0.03
+group_by_length = True
+save_steps = 25
+logging_steps = 5
+max_seq_length = None
+packing = False
+device_map = {"": 0}
+
+system_message = "Answer the question followed by the news articles as briefly as possible and with just the extract of the original text from the articles or reply 'N/A' if the answer cannot be found."
+
+import pandas as pd
+from tqdm import tqdm
+
+df = pd.read_csv("newsqa-dataset-combined-all.csv")
+
+# Initialize lists to store prompts and responses
+prompts = []
+responses = []
+
+for idx, row in tqdm(df.iterrows()):
+    prompt = row["Text"] + "\n " + row["Question"] + " " + "Please answer the question by extracting the exact text from the paragraph."
+    prompts += [prompt]
+    responses += [row["Answer"]]
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'prompt': prompts,
+    'response': responses
+})
+
+print('There are ' + str(len(df)) + ' successfully-generated examples. Here are the first few:')
+
+df.head()
+df.dropna(how="any", inplace=True)
+
+"""
+Split the data into train and test sets, with 80% in the train set
+"""
+train_df = df.sample(frac=0.9, random_state=42)
+test_df = df.drop(train_df.index)
+
+# Save the dataframes to .jsonl files
+train_df.to_json('train.jsonl', orient='records', lines=True)
+test_df.to_json('test.jsonl', orient='records', lines=True)
+
+# Load datasets
+train_dataset = load_dataset('json', data_files='train.jsonl', split="train")
+valid_dataset = load_dataset('json', data_files='test.jsonl', split="train")
+
+# Preprocess datasets
+train_dataset_mapped = train_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+valid_dataset_mapped = valid_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+# Set training parameters
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    weight_decay=weight_decay,
+    fp16=fp16,
+    bf16=bf16,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    warmup_ratio=warmup_ratio,
+    group_by_length=group_by_length,
+    lr_scheduler_type=lr_scheduler_type,
+    report_to="all",
+    evaluation_strategy="steps",
+    eval_steps=20  # Evaluate every 20 steps
+)
+# Set supervised fine-tuning parameters
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_dataset_mapped,
+    eval_dataset=valid_dataset_mapped,  # Pass validation dataset here
+    peft_config=peft_config,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=training_arguments,
+    packing=packing,
+)
+trainer.train()
+trainer.model.save_pretrained(new_model)
+
+# Cell 4: Test the model
+logging.set_verbosity(logging.CRITICAL)
+prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\nWrite a function that reverses a string. [/INST]" # replace the command here with something relevant to your task
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
+result = pipe(prompt)
+print(result[0]['generated_text'])
